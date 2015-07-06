845568599cf8cdf6e96ee19d81a04b9c1261ad47
dominik <dominik@dominik-K53SV>
2015-07-06 14:13:31 +0200

    first commit

---
+++ main_c.c
@@ -0,0 +1,62 @@
+/* 
+ * File:   main_c.c
+ * Author: dominik
+ *
+ * Created on 6 lipiec 2015, 09:27
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 1501
+#define ADDRESS "127.0.0.1"
+
+int main(int argc, char *argv[]) {
+    int sock;
+    struct sockaddr_in server;
+    char message[128], server_reply[2000];
+
+    //Create socket
+    if (sock = socket(AF_INET, SOCK_STREAM, 0) == -1) {
+        perror("socket");
+        return EXIT_FAILURE;
+    }
+
+    //Prepare structure sockaddr_in
+    server.sin_family = AF_INET; // rodzaj gniazda z ktorego kozysta TCP/IP
+    server.sin_port = htons(PORT); // numer portu
+    server.sin_addr.s_addr = inet_addr(ADDRESS);
+
+    //Connect to remote server
+    if (connect(sock, (struct sockaddr *) &server, sizeof (server)) < 0) {
+        perror("connect");
+        //return EXIT_FAILURE;
+    }
+    
+    //keep communicating with server
+    while (1) {
+        printf("Enter message : ");
+        scanf("%s", message);
+
+        //Send some data
+        if (send(sock, message, strlen(message), 0) < 0) {
+            puts("Send failed");
+            return 1;
+        }
+
+        //Receive a reply from the server
+        if (recv(sock, server_reply, 2000, 0) < 0) {
+            puts("recv failed");
+            break;
+        }
+
+        puts("Server reply :");
+        puts(server_reply);
+    }
+
+    close(sock);
+    return (EXIT_SUCCESS);
+}
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=klient
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/klient
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=klient.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/klient.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=klient
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/klient
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=klient.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/klient.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
